5e74a5c0919a9d76558d48baef372b8174b8cf15
Yeojin Kim <yeojin.dev@gmail.com>
2020-07-19 00:30:10 +0900

    테스트 환경에서 파일 업로드 폴더 생성

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,9 @@
+import os
+
 import pytest
 
 from app import (
+    BASE_FOLDER,
     app,
     db,
 )
@@ -8,6 +11,9 @@ from app import (
 
 @pytest.fixture
 def client():
+    upload_path = os.path.join(BASE_FOLDER, os.environ['UPLOAD_FOLDER'])
+    os.makedirs(upload_path, exist_ok=True)
+
     client = app.test_client()
     yield client
     db.homework.remove({})
