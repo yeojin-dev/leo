d12b09cff2f094ba39ca390d2afd35f37104f0fb
Yeojin Kim <yeojin.dev@gmail.com>
2020-07-18 23:25:56 +0900

    플레이스홀더 없는 f-string 제거

--- tests/test_app.py
+++ tests/test_app.py
@@ -13,7 +13,7 @@ def test_submit_homework(client):
     password = client.application.config['UPLOAD_PASSWORD']
     data = {'title_give': title, 'author_give': author, 'password_give': password}
 
-    response = client.post(f'/homework', data=data)
+    response = client.post('/homework', data=data)
     assert response.status_code == HTTPStatus.OK
     assert response.json['result'] == 'success'
 
@@ -24,7 +24,7 @@ def test_submit_homework_with_a_wrong_password(client):
     password = 'wrong_password'
     data = {'title_give': title, 'author_give': author, 'password_give': password}
 
-    response = client.post(f'/homework', data=data)
+    response = client.post('/homework', data=data)
     assert response.status_code == HTTPStatus.OK
     assert response.json['result'] == 'fail'
 
