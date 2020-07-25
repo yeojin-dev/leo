20e4bd7dbd409bee8ff26a713f83cfb3d0a443ba
Yeojin Kim <yeojin.dev@gmail.com>
2020-07-25 17:29:31 +0900

    제출한 과제 파일 읽기 API 테스트

--- tests/test_app.py
+++ tests/test_app.py
@@ -25,6 +25,26 @@ def test_submit_and_upload_homework(client):
     assert response.status_code == HTTPStatus.OK
 
 
+def test_read_homework(client):
+    title = 'test html'
+    author = 'tester'
+    password = client.application.config['UPLOAD_PASSWORD']
+    data = {'title_give': title, 'author_give': author, 'password_give': password}
+    client.post('/homework', data=data)
+
+    temp_filepath = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'fixtures', 'temp.html')
+    with open(temp_filepath, 'rb') as temp_file:
+        temp_html: str = temp_file.read().decode()
+
+        temp_file.seek(0)
+        data = {'file': (temp_file, temp_file.name)}
+        client.post(f'/homework/{author}/{title}', data=data, content_type='multipart/form-data')
+
+    response = client.get(f'/homework/{author}/{title}')
+    assert response.status_code == HTTPStatus.OK
+    assert temp_html == response.data.decode()
+
+
 def test_submit_homework_with_a_wrong_password(client):
     title = 'test html'
     author = 'tester'
